93a3340e862708a41e102c303b9706a9f279ea13
fdoargolo <felipeargolo75@gmail.com>
2024-05-06 00:11:04 -0300

    function id checker mecanism that also improve the salary increase mechanism

--- src/employeeRegistration/Application.java
+++ src/employeeRegistration/Application.java
@@ -11,53 +11,69 @@ public class Application {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scanner = new Scanner(System.in);
-		
+
 		List<Employee> employees = new ArrayList<>();
-		
+
 		System.out.println("How many employees will be registered?");
 		int numberEmployees = scanner.nextInt();
-		
 
-		for(int i = 0; i<numberEmployees; i++) {
+		for (int i = 0; i < numberEmployees; i++) {
 			scanner.nextLine();
-			System.out.println("Employee " +(i+1) +"#:");
+			System.out.println("Employee " + (i + 1) + "#:");
 			System.out.print("ID: ");
 			int id = scanner.nextInt();
+			
+			while (idChecker(employees, id) != null) {
+				System.out.println("ID already taken, try again:");
+				System.out.print("ID: ");
+				id = scanner.nextInt();
+			
+			}
+			
 			scanner.nextLine();
 			System.out.print("Name: ");
 			String name = scanner.nextLine();
 			System.out.print("Salary:");
 			double salary = scanner.nextDouble();
-			
-			Employee employeeData = new Employee(id,name,salary);
+
+			Employee employeeData = new Employee(id, name, salary);
 			employees.add(employeeData);
 			System.out.println();
 		}
-		
+
 		System.out.print("Enter the employee id that will have salary increase: ");
 		int idIncrease = scanner.nextInt();
 		scanner.nextLine();
-		System.out.print("Enter the porcentage:");
-		double percentage = scanner.nextDouble();
+
+		//for (Employee employee : employees) {
+			//if (employee.getId() == idIncrease) {
+				//employee.salaryIncrease(percentage);
+			//}erro aqui
+		//}
 		
-		for (Employee employee: employees) {
-			if(employee.getId() == idIncrease) {
-				employee.salaryIncrease(percentage);
-				break; 
-			}
-			else {
-				System.out.println("This ID does not exist!");
-			}
-		}
-		
+		Employee result = idChecker(employees, idIncrease);
+		if(result != null) {
+			System.out.print("Enter the porcentage:");
+			double percentage = scanner.nextDouble();
+			result.salaryIncrease(percentage);
+		}else {
+			System.out.println("ID does not exist.");
+		}// verificar isso aqui
+
 		System.out.println("List of employees: ");
-		
-		for (Employee employee: employees) {
+
+		for (Employee employee : employees) {
 			System.out.println(employee);
 		}
-	
-		
+
 		scanner.close();
 	}
+
+	public static Employee idChecker(List<Employee> list, int id) {
+		Employee result = list.stream().filter(x -> x.getId() == id).findFirst().orElse(null);
+		return result;
+	}
+	
+	
 
 }
